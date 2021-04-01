afac880e50607503f19d7ed8a4c7ace20d87200e
andreu <andreu@fedora.localdomain>
2021-04-01 13:21:13 +0200

    TG-3 #in-progress

--- nextcloud-server:v1/startup.sh
+++ nextcloud-server:v1/startup.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 # Nextcloud server
 
-apt -y install apache2 mariadb-server libapache2-mod-php7.4 && echo "Basics OK"
-apt -y install php7.4-gd php7.4-mysql php7.4-curl php7.4-mbstring php7.4-intl && echo "PHP OK"
-apt -y install php7.4-gmp php7.4-bcmath php-imagick php7.4-xml php7.4-zip && "Extra PHP OK"
-
 cp /opt/docker/nextcloud.conf /etc/apache2/sites-available/
 
 /etc/init.d/mysql start
 
 # Series de comandes al MariaDB
-mysql -u root -p '' < sqlconfig.sql
+mysql -u root < sqlconfig.sql
+
+echo "root:jupiter" | chpasswd
 
 wget https://download.nextcloud.com/server/releases/nextcloud-21.0.0.tar.bz2.sha256
 sha256sum -c nextcloud-21.0.0.tar.bz2.sha256 < nextcloud-21.0.0.tar.bz2
