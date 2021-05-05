825a14a7f7736e2e548763fa3d8e82401fafb85e
carlesgo17 <81510515+carlesgo17@users.noreply.github.com>
2021-05-05 12:11:55 +0200

    Add files via upload

--- nextcloud-server:v3/startup.sh
+++ nextcloud-server:v3/startup.sh
@@ -17,22 +17,22 @@ mysql -u root < sqlconfig.sql
 echo "root:jupiter" | chpasswd
 
 echo -e "\t \t \e[34m Descàrrega dels fitxers \e[0m"
-wget https://download.nextcloud.com/server/releases/nextcloud-21.0.0.tar.bz2
-wget https://download.nextcloud.com/server/releases/nextcloud-21.0.0.tar.bz2.asc
+wget https://download.nextcloud.com/server/releases/nextcloud-21.0.1.tar.bz2
+wget https://download.nextcloud.com/server/releases/nextcloud-21.0.1.tar.bz2.asc
 wget https://nextcloud.com/nextcloud.asc
 
 echo -e "\t \t \e[34m Comprovació del checksum \e[0m"
-wget https://download.nextcloud.com/server/releases/nextcloud-21.0.0.tar.bz2.sha256
-sha256sum -c nextcloud-21.0.0.tar.bz2.sha256 < nextcloud-21.0.0.tar.bz2
+wget https://download.nextcloud.com/server/releases/nextcloud-21.0.1.tar.bz2.sha256
+sha256sum -c nextcloud-21.0.1.tar.bz2.sha256 < nextcloud-21.0.1.tar.bz2
 sleep 1
 
 echo -e "\t \t \e[34m Verifiquem clau GPG \e[0m"
 gpg --import nextcloud.asc
-gpg --verify nextcloud-21.0.0.tar.bz2.asc nextcloud-21.0.0.tar.bz2
+gpg --verify nextcloud-21.0.1.tar.bz2.asc nextcloud-21.0.1.tar.bz2
 sleep 1
 
 echo -e "\t \t \e[34m Extraent els arxius necessaris... \e[0m"
-pv nextcloud-21.0.0.tar.bz2 | tar -xj && echo -e "\t \t \e[34m Extracció completada \e[0m"
+pv nextcloud-21.0.1.tar.bz2 | tar -xj && echo -e "\t \t \e[34m Extracció completada \e[0m"
 
 cp -r nextcloud /var/www
 a2ensite nextcloud.conf
